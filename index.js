e5a1a3ec1f861692bf024a64662f01aa40cd1d09
James Mburu Githire <jamesmburug24@gmail.com>
2022-08-21 11:55:44 +0300

    First few passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payRatePerHour]){
+    return ({
+        "firstName":firstName,
+        "familyName":familyName,
+        "title":title,
+        "payRatePerHour":payRatePerHour,
+        "timeInEvents":[],
+        "timeOutEvents":[]
+    })
+}
+
+function createEmployeeRecords(mainArray){
+    mainArray.forEach(element => {
+        createEmployeeRecord(element);
+    });
+}
+
+function createTimeInEvent(empObj, dateStmp){
+    let arr = dateStmp.split(" ");
+    return empObj.timeInEvents.push({
+        "TimeIn":{
+            "date":arr[0],
+            "time":arr[1]
+        } 
+    })
+}
